7b56f401855cede97b9aca84aae42d1b8c2f0749
Aman1998 <1998.amangeldi@gmail.com>
2023-01-17 19:01:28 +0600

    refactoring: change jsx element to fc generic type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { FC } from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 import '@common/styles/layout.scss';
 
-const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
   <>
     <Head>
       <title>NextJS App From Scratch</title>
