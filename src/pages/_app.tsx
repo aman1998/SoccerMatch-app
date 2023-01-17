9f032e2b2b2bb20771be022f0eebf8c1a1fb9362
Aman1998 <1998.amangeldi@gmail.com>
2023-01-17 18:42:17 +0600

    change: update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Head from 'next/head';
+import { AppProps } from 'next/app';
 import '@common/styles/layout.scss';
 
-const MyApp = ({ Component, pageProps }: any): JSX.Element => (
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
   <>
     <Head>
       <title>NextJS App From Scratch</title>
