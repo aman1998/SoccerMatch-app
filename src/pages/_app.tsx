ca675d3b39be88bcff91f58daca8a096d86b0b2e
Aman1998 <1998.amangeldi@gmail.com>
2023-01-19 23:09:52 +0600

    add progressbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
+import NextNProgress from 'nextjs-progressbar';
 
 import { wrapper } from '@store/index';
 
@@ -15,6 +16,7 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
       <Head>
         <title>NextJS App From Scratch</title>
       </Head>
+      <NextNProgress color="#1e9e70" height={1} showOnShallow={true} />
       <Component {...pageProps} />
     </Provider>
   );
