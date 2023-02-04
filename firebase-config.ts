0498f9d1dfcdba7a4f27c532632e284a7a0e4baf
Aman1998 <1998.amangeldi@gmail.com>
2023-02-04 22:09:41 +0600

    env fixed

--- firebase-config.ts
+++ firebase-config.ts
@@ -2,14 +2,15 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
+console.log('key =>', process.env.NEXT_PUBLIC_API_KEY);
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: 'highlights-61135',
-  storageBucket: 'highlights-61135.appspot.com',
-  messagingSenderId: '179118235142',
-  appId: '1:179118235142:web:0533893c1349d491a82d87',
-  measurementId: 'G-Z3B7K65K3B',
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 export const database = getFirestore(initializeApp(firebaseConfig));
